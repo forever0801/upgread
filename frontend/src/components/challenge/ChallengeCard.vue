b868a675fcf0af5291dd8ad8f93687889d50dadd
forever0801 <alwaysna0813@naver.com>
2024-11-30 18:30:49 +0900

    fix: challengeCard 정보 추가

--- frontend/src/components/challenge/ChallengeCard.vue
+++ frontend/src/components/challenge/ChallengeCard.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="card my-2">
     <div class="card-body">
-      <h5 class="card-title">7일간 100페이지 읽기</h5>
-      <p class="card-text">25명 참여중</p>
-      <a href="#" class="btn btn-primary">참여하기</a>
+      <h5 class="card-title">2024 독서모임</h5>
+      <div class="row">
+        <div class="col-8">
+          <h6 class="card-subtitle mb-2 text-muted">25명 참여중</h6>
+          <p class="card-text">올해에는 책을 좀 읽어봅시다</p>
+        </div>
+        <div class="col-4">
+          <h6 class="card-subtitle mb-2 text-muted">기간 : 7일</h6>
+          <h6 class="card-subtitle mb-2 text-muted">목표 : 100페이지</h6>
+
+        </div>
+      </div>
+      <a href="#" class="btn btn-primary mt-2">참여하기</a>
     </div>
   </div>
 </template>
